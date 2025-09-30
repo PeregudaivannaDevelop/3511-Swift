5123fc219549f8ec3e427d8b6f39b314f0a4b7e8
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-09-30 12:26:51 +0300

    init

---
+++ ChicknFarmBrawl/App/ChicknFarmBrawlApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChicknFarmBrawlApp.swift
+//  ChicknFarmBrawl
+//
+//  Created by Serhii Babchuk on 30.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct ChicknFarmBrawlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            CFLaunchView()
+        }
+    }
+}
