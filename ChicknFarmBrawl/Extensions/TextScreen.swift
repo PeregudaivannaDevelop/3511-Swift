1c8d5391ccd1a04d84157c4c3b21c3f831698d13
SBabchukTM <babchuk.serhii@trident-media.agency>
2025-11-11 11:56:24 +0200

    fix

--- ChicknFarmBrawl/Extensions/TextScreen.swift
+++ ChicknFarmBrawl/Extensions/TextScreen.swift
@@ -14,7 +14,7 @@ enum TypeScreen {
         switch self {
         case .policy:
                 """
-                This privacy policy applies to the Chick’n Farm Brawl app (hereby referred to as "Application") for mobile devices that was created by Emily Johnson (hereby referred to as "Service Provider") as a Free service. This service is intended for use "AS IS".
+                This privacy policy applies to the Farmstead Brawl app (hereby referred to as "Application") for mobile devices that was created by Emily Johnson (hereby referred to as "Service Provider") as a Free service. This service is intended for use "AS IS".
 
                 Information Collection and Use
 
